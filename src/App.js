a006f233e787160154caf0794707d5b4e6df02a2
Longo <plongo@indracompany.com>
2019-11-04 17:53:42 -0300

    Header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header'
+
+import Routes from './routes';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello</h1>
-            </header>
-        </div>
+        <BrowserRouter>
+            <GlobalStyle />
+            <Header />
+            <Routes />
+        </BrowserRouter>
     );
 }
 
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,7 @@
-import React from './node_modules/react';
+import React from 'react';
 
 // import { Container } from './styles';
 
 export default function Cart() {
-    return <div />;
+    return <h1>Cart</h1>;
 }
