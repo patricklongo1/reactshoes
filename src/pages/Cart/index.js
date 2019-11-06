7837a929cfe011061a5ba1d97416b9432fb688a6
Longo <plongo@indracompany.com>
2019-11-06 17:54:03 -0300

    calculo de totais done

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,12 +6,11 @@ import {
     MdAddCircleOutline,
     MdDelete,
 } from 'react-icons/md';
-
 import { Container, ProductTable, Total } from './styles';
-
 import * as CartActions from './actions';
+import { formatPrice } from '../../util/format';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
     function increment(product) {
         updateAmount(product.id, product.amount + 1);
     }
@@ -70,7 +69,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                                 </div>
                             </td>
                             <td>
-                                <strong>R$259,80</strong>
+                                <strong>{product.subtotal}</strong>
                             </td>
                             <td>
                                 <button
@@ -90,7 +89,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
                 <Total>
                     <span>TOTAL:</span>
-                    <strong>R$1920,27</strong>
+                    <strong>{total}</strong>
                 </Total>
             </footer>
         </Container>
@@ -98,7 +97,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = state => ({
-    cart: state.cart,
+    cart: state.cart.map(product => ({
+        ...product,
+        subtotal: formatPrice(product.price * product.amount),
+    })),
+    total: formatPrice(
+        state.cart.reduce((total, product) => {
+            return total + product.price * product.amount;
+        }, 0)
+    ),
 });
 
 const mapDispatchToProps = dispatch =>
